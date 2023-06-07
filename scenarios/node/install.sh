7ddc8cea3e974a381aeacb984969ba7d746b7090
Leonardo Thibes <leonardothibes@gmail.com>
2023-06-07 11:57:28 -0300

    wip

--- scenarios/node/install.sh
+++ scenarios/node/install.sh
@@ -2,3 +2,7 @@
 
 echo "Installing Node.js development scenario..."
 BASE=$(dirname $0)/../base
+REPO=https://github.com/leonardothibes/multipass/archive/refs/heads/feature/node-scenario.zip
+TEMP=/tmp
+
+curl -o $REPO $TEMP
