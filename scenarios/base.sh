883425555d5e914fe9e1e38a4a0e5b22fa3473bc
Leonardo Thibes <leonardothibes@gmail.com>
2023-06-07 12:48:58 -0300

    wip

--- scenarios/base.sh
+++ scenarios/base.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 
-echo "Installing Base development scenario..."
+echo "Installing Base development scenario"
+
+function update()
+{
+    echo " - Updating package list..."
+
+    LOCK=/tmp/lock.base.update
+    [ -f ${LOCK} ] && return
+
+    sudo apt update > /dev/null 2>&1
+    > ${LOCK}
+}
+
+function utils()
+{
+    echo " - Installing utils..."
+
+    LOCK=/tmp/lock.base.utils
+    [ -f ${LOCK} ] && return
+
+    sudo apt install -y git-flow \
+                        make     \
+                        zip      \
+    > /dev/null 2>&1
+    
+    > ${LOCK}
+}
+
+update
+utils
