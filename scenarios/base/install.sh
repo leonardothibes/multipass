58d2cf67adba0b7529132e350591d6ca1eaa25e9
Leonardo Thibes <leonardothibes@gmail.com>
2023-06-07 11:49:34 -0300

    wip

--- scenarios/base/install.sh
+++ scenarios/base/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "Installing base development scenario..."
+HERE=$(dirname $0)
+
+for DIR in $(ls ${HERE}/scripts)
+do
+    SCRIPT="${HERE}/scripts/${DIR}/run.sh"
+    chmod 755 ${SCRIPT}
+    sh ${SCRIPT}
+done
--- scenarios/node/install.sh
+++ scenarios/node/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "Installing Node.js development scenario..."
+BASE=$(dirname $0)/../base
